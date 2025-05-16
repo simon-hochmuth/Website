d04116f2bf87e0effb1dc5c7d5b08fa2e885c8cb
simon-hochmuth <simon.hochmuth@gmail.com>
2025-05-16 18:31:17 -0400

    added updates to project section

---
+++ src/views/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="page">
+      <h2 class="major">Projects</h2>
+      <ul>
+        <li>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Deep_Learning_Ship_Dataset.ipynb" target="_blank">
+            Ship Prediction in Satellite Images
+          </a>
+        </li>
+        <!-- add more projects here -->
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .page {
+    padding: 2rem;
+  }
+  </style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="page">
+      <h2 class="major">Projects</h2>
+      <ul>
+        <!-- Work -->
+        <li>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Deep_Learning_Ship_Dataset.ipynb" target="_blank">
+            Ship Prediction in Satellite Images : Using Deep Learning Neural Network Strategies to Predict Whether Satellite Images Contain Ships.
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Credit_card_dataset.ipynb" target="_blank">
+            Grouping of Credit Accounts : Developing Marketing Strategies using Unsupervised Machine Learning
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Supervised_Learning_Wine.ipynb" target="_blank">
+            Predicting Wine Rankings  :  Supervised Machine Learning to Predict Wine Rankings
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/SQL_in_Python.ipynb" target="_blank">
+            Dataset Manipulation Using SQL
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Capstone_1_Military_Expenditure%20(2).ipynb" target="_blank">
+            Global Milatary Expenditure : Data Exploration of Global Milatary Expenditure
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Projects_From_Masters_Courses/Reinforcement_Learning.pdf" target="_blank">
+            Reinforcement Learning : Reinforcement Learning to locate shortest paths
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Projects_From_Masters_Courses/Deep_Learning_MNIST_FASHION.pdf" target="_blank">
+            Deep Learning : MNIST Fashion Dataset
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Projects_From_Masters_Courses/Log_Regression_and_Data_Mining.pdf" target="_blank">
+            Regression & Data Mining : Using machine learning models to predict when a device will fail, thus showing where predictive maintenance can happen
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Projects_From_Masters_Courses/SVM_Models.pdf" target="_blank">
+            SVM Models : Using machine learning models to predict when a device will fail, thus showing where predictive maintenance can happen
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Projects_From_Masters_Courses/Decision_Tree_Models_with_Cognos.pdf" target="_blank">
+            Decision Tree Models  :  utilizing IBM Cognos software to analyze data on billionaires to uncover trends
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Projects_From_Masters_Courses/Neural_Networks.pdf" target="_blank">
+            Neural Networks : Neural Networks to understand connections between diabetes and Pima Indians
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Projects_From_Masters_Courses/K-means%20Clustering.pdf" target="_blank">
+            KMeans Clustering : K means clustering to classify satellite data related to soil
+          </a>
+          <a href="https://github.com/simon-hochmuth/portfolio/blob/master/Projects_From_Masters_Courses/Ensemble_Models.pdf" target="_blank">
+            Ensemble Models : Using Ensemble models to predict churn in a company
+          </a>
+
+
+
+        </li>
+        <!-- add more projects here -->
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .page {
+    padding: 2rem;
+  }
+  </style>
