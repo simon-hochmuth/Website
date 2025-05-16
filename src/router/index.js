baa4579406314ae4c925df63a237746f2b3d551b
simon-hochmuth <simon.hochmuth@gmail.com>
2025-05-16 18:19:45 -0400

    adding routing to new pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import Home from '../views/Home.vue'
+import Projects from '../views/Projects.vue'
+import School from '../views/School.vue'
+import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
+  { path: '/', component: Home },
+  { path: '/projects', component: Projects },
+  { path: '/school', component: School },
+  { path: '/about', component: About },
+  { path: '/contact', component: Contact }
 ]
 
 const router = createRouter({
